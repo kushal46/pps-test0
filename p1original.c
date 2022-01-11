c4d185024b5476891c45a0c106b03d9b4d020d41
kush46 <97089328+kushal46@users.noreply.github.com>
2022-01-11 17:04:28 +0000

    null

--- p1original.c
+++ p1original.c
@@ -1,2 +1,14 @@
+#include<stdio.h>
+int main()
 
+{
+  int num1,num2,sum;
+  printf("enter any two numbers:\n");
+  scanf("%d%d",&num1,&num2);
+  sum=num1+num2;
+  printf("sum of %d + %d is %d\n",num1,num2,sum);
 }
+
+
+
+
